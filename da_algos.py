6c5410dfc75f1311be4f6d03c09ed2eea783e19f
Ajay Krishnan <42097993+ajaykrishnan23@users.noreply.github.com>
2021-04-04 22:22:37 +0530

    6x Speedup

--- da_algos.py
+++ da_algos.py
@@ -1,10 +1,10 @@
 from argparse import ArgumentParser
 from termcolor import colored
+from numba import jit,njit
 from tqdm import tqdm
 import scipy.spatial.distance as dist
 import numpy as np
 import random
-
 
 class DaTechniques:
   def __init__(self, subset_size, filenames, embeddings, sample_size=None):
@@ -14,22 +14,31 @@ class DaTechniques:
     self.subset_size = subset_size
     self.sample_size = sample_size
 
+
   def farthest_point(self):
     
     centroid = sum(self.embeddings)/len(self.embeddings)
     distances = [dist.euclidean(i, centroid) for i in self.embeddings]
     return distances.index(max(distances))
 
-  def min_max_diverse_embeddings(self, i = None) :
-
+  def min_max_diverse_embeddings(self):
+    #calls Diversity_Algorithm
     if len(self.embeddings) != len(self.filenames) or len(self.embeddings) == 0 :
-        return 'Data Inconsistent'
-    n = int(self.subset_size * len(self.embeddings))
+      return 'Data Inconsistent'
+    else:
+      return self.compute_diversity(self.filenames, self.embeddings, self.subset_size, i=self.farthest_point())
+
+
+  @staticmethod
+  @jit(nopython=True)
+  def compute_diversity(filenames, embeddings, subset_size, i = None) :
+
+    n = int(subset_size * len(embeddings))
     # print("Len of Filenames and Feature List for sanity check:",len(filenames),len(feature_list))
     print("Running DA Standard..")
     print("Subset_Size:",n)
-    filename_copy = self.filenames.copy()
-    set_input = self.embeddings.copy()
+    filename_copy = filenames
+    set_input = embeddings
     set_output = []
     filename_output = []
     idx = 0
@@ -37,33 +46,48 @@ class DaTechniques:
         idx = random.randint(0, len(set_input) -1)
     else:
         idx = i
+    # set_output = np.append(set_output,set_input[idx])
     set_output.append(set_input[idx])
     filename_output.append(filename_copy[idx])
-    min_distances = [1000] * len(set_input)
+    min_distances = np.full(len(set_input),1000.0)
     # maximizes the minimum distance
-    for _ in tqdm(range(n - 1)):
+    count = 10
+    for _ in range(n - 1):
+        if _>count:
+          print(count,"Samples Done")
+          count+= 500
         for i in range(len(set_input)) :
-            # distances[i] = minimum of the distances between set_output and one of set_input
             dist = np.linalg.norm(set_input[i] - set_output[-1])
             if min_distances[i] > dist :
                 min_distances[i] = dist
-        inds = min_distances.index(max(min_distances))
+        inds = np.argmax(min_distances)
+        # inds = min_distances.index(max(min_distances))
+        # print(inds)
         set_output.append(set_input[inds])
         filename_output.append(filename_copy[inds])
-
     return filename_output, set_output, min_distances
 
-  def min_max_diverse_embeddings_fast(self, i = None) :
-
+  def min_max_diverse_embeddings_fast(self):
+    #calls Diversity_Algorithm
     if len(self.embeddings) != len(self.filenames) or len(self.embeddings) == 0 :
-        return 'Data Inconsistent'
-    n = int(self.subset_size * len(self.embeddings))
-    sample_size = int(self.sample_size * len(self.filenames))
+      return 'Data Inconsistent'
+    else:
+      return self.compute_diversity_fast(self.filenames, self.embeddings, self.subset_size, self.sample_size, i=self.farthest_point())
+
+
+  @staticmethod
+  @jit(nopython=True)
+  def compute_diversity_fast(filenames, embeddings, subset_size, sample_size, i = None) :
+    def random_sample(embeddings_len, sample):
+      return np.random.choice(embeddings_len,sample,replace=False)
+      
+    n = int(subset_size * len(embeddings))
+    sample_size = int(sample_size * len(filenames))
+    print("Running DA Standard..")
     print("Subset Size:",n)
     print("Sample Size:",sample_size)
-    # print("Len of Filenames and Feature List for sanity check:",len(filenames),len(feature_list))
-    filename_copy = self.filenames.copy()
-    set_input = self.embeddings.copy()
+    filename_copy = filenames
+    set_input = embeddings
     set_output = []
     filename_output = []
     idx = 0
@@ -73,17 +97,20 @@ class DaTechniques:
         idx = i
     set_output.append(set_input[idx])
     filename_output.append(filename_copy[idx])
-    min_distances = [1000] * len(set_input)
+    min_distances = np.full(len(set_input),1000.0)
     # maximizes the minimum distance
-    count = 0
-    for _ in tqdm(range(n - 1)):
-      # min_distances[inds] = (set_input[inds] - set_output[-1]) #incase the index value of maximum index does not get selected
-      sampled_inds = random.sample(range(0,len(self.embeddings)),sample_size)
+    count = 10
+    for _ in (range(n - 1)):
+      #tqdm alternative
+      if _>count:
+        print(count,"Samples Done")
+        count+= 500
+      sampled_inds = random_sample(len(embeddings),sample_size)
       for idx in sampled_inds :  
           dist = np.linalg.norm(set_input[idx] - set_output[-1])
           if min_distances[idx] > dist :
               min_distances[idx] = dist
-      inds = min_distances.index(max(min_distances))
+      inds = np.argmax(min_distances)
       set_output.append(set_input[inds])
       filename_output.append(filename_copy[inds])
       dist = np.linalg.norm(set_input[inds] - set_output[-1])
